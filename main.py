d4d7959481a17d93b6680f3a348dad52af801cf5
Valery Yushchenko <valery.yushchenko@gmail.com>
2010-07-16 09:05:17 +0300

    article & article page templates

--- main.py
+++ main.py
@@ -8,18 +8,42 @@ from google.appengine.ext.webapp import template
 def path(name):
     return os.path.join(os.path.dirname(__file__), name)
 
+def article_path(article):
+    return os.path.join(os.path.dirname(__file__), 'content/articles', article['file'])
+
 class HomePage(webapp.RequestHandler):
+
     def get(self):
 
         import articles
 
-        for article in articles.list:
-            article_template_path = os.path.join(os.path.dirname(__file__), 'content/articles', article['file'])
-            article['content'] = template.render(article_template_path, article)
+        for a in articles.list:
+            a['path'] = article_path(a)
+            a['link'] = '/blog/' + a['id']
+            a['content'] = template.render('templates/article.html', { 'article': a })
 
         values = { 'articles': articles.list }
         
         self.response.out.write(template.render(path('templates/home.html'), values))
+
+
+class ArticlePage(webapp.RequestHandler):
+
+    def get_article_by_id(self, id):
+
+        import articles
+
+        for a in articles.list:
+            if  a['id'] == id:
+                a['path'] = article_path(a)
+                a['link'] = '/blog/' + a['id']
+                return a
+
+    def get(self, id):
+        article =  self.get_article_by_id(id)
+        self.response.out.write(template.render( path('templates/article_page.html'),
+                                                 {'article': article} ))
+
 
 class AboutPage(webapp.RequestHandler):
     def get(self):
@@ -27,9 +51,12 @@ class AboutPage(webapp.RequestHandler):
         self.response.out.write(template.render(path('templates/about.html'), values))
 
 
-application = webapp.WSGIApplication([('/', HomePage),
-                                      ('/about/', AboutPage)],
-                                     debug=True);        
+application = webapp.WSGIApplication([
+    ('/',             HomePage),
+    ('/about/',       AboutPage),
+    (r'/blog/(.*)',   ArticlePage)
+], debug=True);        
+
 
 def main():
     run_wsgi_app(application)
