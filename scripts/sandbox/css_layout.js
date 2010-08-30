cccd3e6fbe227f1fa89c1a083c96b4812a259afa
Valery Yushchenko <valery.yushchenko@gmail.com>
2010-08-30 09:11:39 +0300

    css layout game: add and delete blocks

--- scripts/sandbox/css_layout.js
+++ scripts/sandbox/css_layout.js
@@ -1,1 +1,42 @@
-$(function() {  });
+$(function() {
+    
+    function selectElement(jq) {
+        $('.selectable').removeClass('selected');
+        jq.addClass('selected');        
+    }
+
+    function addNewElement(jqContainer) {
+
+        var markup = '<div class="selectable"></div>',
+            jq = $(markup);
+
+        jq.appendTo(jqContainer);
+        selectElement(jq);
+    }
+
+    function removeElement(jq) {
+
+        if (jq.attr('id') === 'root') {
+            return;
+        }
+
+        selectElement(jq.parent());
+        jq.remove();
+    }
+
+    // Events
+
+    $('.selectable').live('click', function() {
+        selectElement($(this));
+    });
+
+    $('#addChild').click(function() {
+        addNewElement($('.selected'));
+    });
+
+
+    $('#remove').click(function() {
+        removeElement($('.selected'));
+    });
+
+});
